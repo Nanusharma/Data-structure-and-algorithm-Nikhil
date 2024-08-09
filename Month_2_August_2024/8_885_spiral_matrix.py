3aaf09da5ea1e2cf4ad06d547f825f23093331e5
Nanusharma <ns0240074@gmail.com>
2024-08-10 00:14:27 +0530

    upddate

---
+++ Month_2_August_2024/8_885_spiral_matrix.py
@@ -0,0 +1,15 @@
+# https://leetcode.com/problems/spiral-matrix-iii/description/?envType=daily-question&envId=2024-08-08
+from typing import List
+# class Solution:
+#     # def spiralMatrixIII(self, rows: int, cols: int, rStart: int, cStart: int) -> List[List[int]]:
+#     def spiralMatrixIII(self):
+#         lst =[]
+#         for i in range(1):
+#             for j in range(1,3):
+#                 lst.append([i,j])
+#         return lst
+# obj = Solution()
+# print(obj.spiralMatrixIII())
+
+s= [0,1,0]
+print(len(s))
