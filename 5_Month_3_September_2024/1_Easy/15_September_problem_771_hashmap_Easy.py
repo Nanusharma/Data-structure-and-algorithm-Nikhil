17ec449deecf77cd9bf028b473ccdc72d60d0647
Nanusharma <ns0240074@gmail.com>
2024-09-15 23:45:52 +0530

    add file 15_september_problem_771_hashmap_Easy.py

---
+++ 5_Month_3_September_2024/1_Easy/15_September_problem_771_hashmap_Easy.py
@@ -0,0 +1,17 @@
+# Problem number 771 Easy Company tags: META
+# https://leetcode.com/problems/jewels-and-stones/
+
+class Solution:
+    def numJewelsInStones(self, jewels: str, stones: str) -> int:
+        hash_table = []
+        for i in stones:
+            if i in hash_table:
+                hash_table[i]+=1
+            else:
+                hash_table=1
+        count=0
+        for i in jewels:
+            if i in hash_table:
+                count+=hash_table[i]
+        return count
+
