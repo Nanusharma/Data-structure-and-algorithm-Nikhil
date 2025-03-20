00943d532f84389f67527376616f230ba19654a4
Nanusharma <ns0240074@gmail.com>
2025-03-20 10:41:51 +0530

    Added some code

---
+++ 11_Month_7_March_2025/3191_Number_Of_Operations_Requires_to_Make_Binary_Array_Equal_to_One.py
@@ -0,0 +1,23 @@
+# https://leetcode.com/problems/minimum-operations-to-make-binary-array-elements-equal-to-one-i/description/?envType=daily-question&envId=2025-03-19
+class Solution:
+    def minOperations(self, nums: List[int]) -> int:
+        oprx = -1
+        count = 0
+        for i in range(len(nums)-2):
+            if nums[i]==0:
+                nums[i] =1
+                if nums[i+1] == 1:
+                    nums[i+1] = 0
+                else:
+                    nums[i+1] =1
+                if nums[i+2] == 1:
+                    nums[i+2] = 0
+                else:
+                    nums[i+2] =1
+                count+=1
+
+        if nums[-1] and nums[-2] and nums[-3] == 1:
+            oprx = count
+            return count
+        else:
+            return oprx
